fa24e9820854b842746761a9dfcbc6789e8c64ee
Mateja Filipović <mateja.filipovic@hunchads.com>
2024-03-13 10:19:05 +0100

    added support for multiple image types

--- app/routes/image_classification.py
+++ app/routes/image_classification.py
@@ -18,10 +18,25 @@ class ImageClassifierResponse(BaseModel):
 @router.post('/predict')
 async def predict(background_tasks: BackgroundTasks, image_file: UploadFile = File(...)):
     image_bytes = await image_file.read()
-    pillow_image = Image.open(io.BytesIO(image_bytes))
+    pillow_image = parse_image(image_bytes)
 
     image_category_prediction = model.predict(pillow_image)
 
     background_tasks.add_task(image_file.file.close)
 
     return ImageClassifierResponse(imageCategory=image_category_prediction)
+
+
+def parse_image(image_bytes: bytes):
+    pillow_image = Image.open(io.BytesIO(image_bytes))
+
+    if (pillow_image.format is not None) and (pillow_image.format.lower() == 'png'):
+        pillow_image = pillow_image.convert('RGBA')
+
+    if pillow_image.mode == 'CMYK':
+        pillow_image = pillow_image.convert('RGB')
+
+    if pillow_image.mode in ('L', 'I;16'):
+        pillow_image = pillow_image.convert('RGB')
+
+    return pillow_image
